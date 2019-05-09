2204ed5dfd8e6fa7c9e240578a821afa32212ae9
hao wang <ge_nah@hotmail.com>
2019-05-09 05:29:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,12 @@
 # Add  code here!
 def prime?(num)
+  if num <= 1 
+    return false
+  end
+  if num == 2
+    return 2
+  end
+  (2...num).each |number| do 
+    if num % number == 0 
+  end
 end
