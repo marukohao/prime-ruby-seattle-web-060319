c2824b0f5e9996c2cdb97a4b3632d696e782f335
hao wang <ge_nah@hotmail.com>
2019-05-09 05:34:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false
+  end
+  if num == 2
+    return true
+  end
+  (2...num).each do |number| 
+    if num % number == 0 
+      return false
+    end
+  end
+  return true
+end
